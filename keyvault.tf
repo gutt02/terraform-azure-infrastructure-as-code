4362758cd5af0322e803ba8fb26512dd73e7da1b
gutt02 <sven.guttmann@gmail.com>
2023-02-09 20:03:20 +0100

    format

--- keyvault.tf
+++ keyvault.tf
@@ -11,7 +11,7 @@ resource "azurerm_key_vault" "this" {
   network_acls {
     default_action = "Deny"
     bypass         = "AzureServices"
-    ip_rules       = [var.client_ip.cidr,var.agent_ip]
+    ip_rules       = [var.client_ip.cidr, var.agent_ip]
   }
 }
 
