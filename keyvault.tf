2003e62f289a9aa9431031b0d4ad8008ea520d51
gutt02 <sven.guttmann@gmail.com>
2023-02-09 16:06:30 +0100

    key vault

---
+++ keyvault.tf
@@ -0,0 +1,23 @@
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/key_vault
+resource "azurerm_key_vault" "this" {
+  name                = "${var.project.customer}-${var.project.name}-${var.project.environment}-kv"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.this.name
+
+  enable_rbac_authorization = true
+  sku_name                  = "standard"
+  tenant_id                 = data.azurerm_client_config.client_config.tenant_id
+
+  network_acls {
+    default_action = "Deny"
+    bypass         = "AzureServices"
+    ip_rules       = var.client_ip.cidr
+  }
+}
+
+# https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/role_assignment
+resource "azurerm_role_assignment" "this" {
+  scope                = azurerm_key_vault.this.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.client_config.object_id
+}
